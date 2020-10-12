41182d60bf9b2156da4f1e707fd81709a3ec3baf
stepin105010 <69056143+stepin105010@users.noreply.github.com>
2020-10-12 23:26:16 +0530

    Update test.c

--- 03_Implementation/test/test.c
+++ 03_Implementation/test/test.c
@@ -1,18 +1,18 @@
 #include <CUnit/Basic.h>
 #include <CUnit/CUnit.h>
 #include<library.h>
-#define PROJECT_NAME    "LMS"
+#define PROJECT_NAME    "LM"
 
 
 
-void testPassword(void);
+void testlogin(void);
 
 int main() {
 
   if (CUE_SUCCESS != CU_initialize_registry())
     return CU_get_error();
   CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
-  CU_add_test(suite, "Password", testPassword);
+  CU_add_test(suite, "login", testlogin);
 
 
   CU_basic_set_mode(CU_BRM_VERBOSE);
@@ -26,10 +26,11 @@ int main() {
   return 0;
 }
 
-void testPassword(void) {
-  CU_ASSERT(1 == Password("pass"));
-  CU_ASSERT(1 == Password("wrong"));//dummy fail case
+void testlogin(void) {
+  CU_ASSERT(1 == login("user","pass"));
+  CU_ASSERT(1 == login("wrong","wrong"));//dummy fail case
 
 
 }
+
 
