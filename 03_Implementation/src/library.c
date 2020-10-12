8fa7ad133b510223d63021c43e6bc7a60f56e7dc
stepin105010 <69056143+stepin105010@users.noreply.github.com>
2020-10-12 23:24:48 +0530

    Update library.c

--- 03_Implementation/src/library.c
+++ 03_Implementation/src/library.c
@@ -1,57 +1,43 @@
 #include<library.h>
-int Password(char password[10])
+int login(char user[10],char pass[10])
 {
+	int a=0,i=0,k;
+    char uname[10],c=' ';
+    char pword[10],code[10];
 
-system("cls");
-char d[25]="Password Protected";
-char ch,pass[10];
-int i=0,j;
-gotoxy(10,4);
-for(j=0;j<20;j++)
-{
-Sleep(50);
-printf("*");
-}
-for(j=0;j<20;j++)
-{
-Sleep(50);
-printf("%c",d[j]);
-}
-for(j=0;j<20;j++)
-{
-Sleep(50);
-printf("*");
-}
-gotoxy(10,10);
-gotoxy(15,7);
-printf("Enter Password:");
 
-while(ch!=13)
-{
-ch=getch();
+   // printf("\n  \xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\  LOGIN FORM  \xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\  ");
+    	printf("\t\t\t\t\tLOGIN");
+	printf(" \n\n                  ENTER USERNAME:-");
+	scanf("%s", uname);
+	printf(" \n\n                  ENTER PASSWORD:-");
+	/*while(i<10)
+	{
+	    pword[i]=getch();
+	    c=pword[i];
+	    if(c==13) break;
+	    else printf("*");
+	    i++;
+	}
+	pword[i]='\0';*/
+	scanf("%s", &pword);
 
-if(ch!=13 && ch!=8){
-putch('*');
-pass[i] = ch;
-i++;
-}
-}
-pass[i] = '\0';
-if(strcmp(pass,password)==0)
-{
+	i=0;
 
-gotoxy(15,9);
-printf("Password match");
-gotoxy(17,10);
-printf("Press any key to countinue.....");
-getch();
-p=1;
+    	//if((uname=="user")&&(pword=="pass"))
+	//if((uname,"user")==0 && strcmp(pword,"pass")==0)
+	if(strcasecmp(uname,"user")==0 && strcasecmp(pword,"pass")==0)
+	{
+	k=1;
+
+	}
+	else
+	{
+		k=0;
+
+
+	}
+
+    return k;
+
 }
-else
-{
-gotoxy(15,16);
-printf("\aWarning!! Incorrect Password");
-getch();
-p=0;
-}
-return p;
