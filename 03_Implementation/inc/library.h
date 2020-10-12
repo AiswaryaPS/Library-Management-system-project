4369135bf974429f904f34c77a21fae18f73095d
stepin105010 <69056143+stepin105010@users.noreply.github.com>
2020-10-12 23:22:06 +0530

    Update library.h

--- 03_Implementation/inc/library.h
+++ 03_Implementation/inc/library.h
@@ -1,27 +1,24 @@
 #ifndef LIBRARY_H_INCLUDED
 #define LIBRARY_H_INCLUDED
-//#include<windows.h>
 #include<stdio.h>
+
 //#include<conio.h>
-#include <stdlib.h>
+
 #include<string.h>
-//#include<ctype.h>
-//#include<dos.h>
-//#include<time.h>
-//#include<bios.h>
-#endif
-void returnfunc(void);
-void mainmenu(void);
-void addbooks(void);
-void deletebooks(void);
-void editbooks(void);
-void searchbooks(void);
-void issuebooks(void);
-void viewbooks(void);
-void closeapplication(void);
-int  getdata();
-int  checkid(int);
-int t(void);
-int Password();
-void issuerecord();
-//void loaderanim();
+
+//#include<process.h>
+
+#include<stdlib.h>
+
+
+int login();
+void mainmenu();
+void add_contact();
+void delete_contact();
+void edit_contact();
+void search_contact();
+void list_contact();
+
+
+#endif // 
+
